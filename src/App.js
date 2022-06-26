d59869f186785e211179c48aa25391749bfc1366
birkan <b.561birkan@gmail.com>
2022-06-26 16:41:28 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.scss";
 import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
     </div>
   );
 }
