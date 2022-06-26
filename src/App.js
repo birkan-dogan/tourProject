0472756d9a6caff54422a4859590bb1268e82b5e
birkan <b.561birkan@gmail.com>
2022-06-26 16:30:45 +0300

    adding navbar and styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import logo from "./logo.svg";
 import "./App.scss";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome react</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit modi
-        eligendi, optio totam nihil voluptatum!
-      </p>
+      <Navbar />
     </div>
   );
 }
